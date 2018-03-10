08fc866b3714d1483164b0fdbf3b40f40b7debaa
babysean_ <sean791022@gmail.com>
2018-03-11 02:06:57 +1300

    add project view , translate to cube object yeah e~~~ :D

--- OpenGL_Project/src/Main.cpp
+++ OpenGL_Project/src/Main.cpp
@@ -207,23 +207,20 @@ int main( void )
 	unsigned int shader = CreateShader( source.VertexSource, source.FragmentSource );
 	glUseProgram( shader );
 
+	glm::mat4  TModel = glm::translate( glm::mat4( 1.0f ), glm::vec3( 5.0f, 3.0f, -1.0f ) );
+
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective( glm::radians( 45.0f ), 4.0f / 3.0f, 0.1f, 100.0f );
-
 	// Or, for an ortho camera :
-	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	//glm::mat4 Projection = glm::ortho(-1.0f,1.0f,-1.0f,1.0f,0.0f,10.0f); 
+	glm::mat4 Projection = glm::perspective( glm::radians( 60.0f ), 640.0f / 480.0f, 0.1f, 100.0f );
 
-	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-		glm::vec3( 0, 1, 3 ), // Camera is at (4,3,3), in World Space
+		glm::vec3( 0, 0, 10 ), // Camera is at (4,3,3), in World Space
 		glm::vec3( 0, 0, 0 ), // and looks at the origin
 		glm::vec3( 0, 1, 0 )  // Head is up (set to 0,-1,0 to look upside-down)
 	);
 
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4( 1.0f );
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+	glm::mat4 mvp = Projection * View * TModel; // Remember, matrix multiplication is the other way around
 
 	glUniformMatrix4fv( glGetUniformLocation( shader, "mvp" ), 1, 0, &mvp[ 0 ][ 0 ] );
 
